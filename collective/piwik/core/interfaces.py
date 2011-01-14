d0f233e7d65c64e563f34110a817880681bc36bb
Markos Gogoulos <mgogoulos@gmail.com>
2011-01-14 14:33:59 +0000

    piwik settings for piwik.core
    svn path=/collective.piwik.core/trunk/; revision=230515

--- collective/piwik/core/interfaces.py
+++ collective/piwik/core/interfaces.py
@@ -11,8 +11,20 @@ _ = MessageFactory('collective.piwik.core')
 class IPiwikSettings(Interface):
     """Piwik settings."""
 
-    piwik_key = schema.TextLine(title=_(u"Piwik API key"),
+    piwik_server = schema.TextLine(title=_(u"Piwik server URL"), 
+                                description=u'url+piwik location. example http://piwik.unweb.me/piwik/ Please include the slash on the end',
                                 required=True,
-                                default = u'',
+                                default = u'http://piwik.unweb.me/piwik/',
+                                )
+    piwik_siteid = schema.TextLine(title=_(u"Piwik Site id"),
+                                description=u'site id, example 12',
+                                required=True,
+                                default = u'12',
+                                )
+
+    piwik_key = schema.TextLine(title=_(u"Piwik API key"),
+                                description=u'piwik API token auth key, or anonymous if no auth',
+                                required=True,
+                                default = u'anonymous',
                                 )
 
